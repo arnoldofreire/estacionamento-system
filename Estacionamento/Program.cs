90e1eec5faf0727c99bcc1daa997dcc2837e9b12
arnoldofreire <arnoldofreire@gmail.com>
2024-08-22 09:34:41 -0400

    Primeiro commit do projeto Estacionamento

---
+++ Estacionamento/Program.cs
@@ -0,0 +1,45 @@
+﻿// Program.cs
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Estacionamento estacionamento = new Estacionamento();
+        bool continuar = true;
+
+        while (continuar)
+        {
+            Console.WriteLine("\nSelecione uma opção:");
+            Console.WriteLine("1 - Adicionar veículo");
+            Console.WriteLine("2 - Remover veículo");
+            Console.WriteLine("3 - Listar veículos");
+            Console.WriteLine("4 - Sair");
+
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    Console.WriteLine("Digite a placa do veículo:");
+                    string placaAdicionar = Console.ReadLine();
+                    estacionamento.AdicionarVeiculo(placaAdicionar);
+                    break;
+                case "2":
+                    Console.WriteLine("Digite a placa do veículo:");
+                    string placaRemover = Console.ReadLine();
+                    estacionamento.RemoverVeiculo(placaRemover);
+                    break;
+                case "3":
+                    estacionamento.ListarVeiculos();
+                    break;
+                case "4":
+                    continuar = false;
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+        }
+    }
+}
---
+++ Estacionamento/Veiculo.cs
@@ -0,0 +1,12 @@
+// Veiculo.cs
+public class Veiculo
+{
+    public string Placa { get; set; }
+    public DateTime HoraEntrada { get; set; }
+
+    public Veiculo(string placa)
+    {
+        Placa = placa;
+        HoraEntrada = DateTime.Now;
+    }
+}
